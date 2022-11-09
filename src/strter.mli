4ed762b28e952d666a9b508da5a87dcdc2043686
Christian Raasteen <dr.rawstone@outlook.com>
2022-11-09 15:00:14 -0500

    fixing MLI

--- src/strter.mli
+++ src/strter.mli
@@ -1,5 +1,13 @@
+open Torch_core
+
 type 'a t (* hide the type 'a list here by not giving it in signature *)
 val emptyset : 'a t
 val add: 'a -> 'a t ->'a t
 val remove : 'a -> 'a t ->  ('a -> 'a -> bool) -> 'a t
 val contains: 'a -> 'a t ->  ('a -> 'a -> bool) -> bool    
+
+val predictImage: Torch_core.Wrapper.Tensor.t -> int
+
+
+
+
