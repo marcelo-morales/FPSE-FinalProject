73b47094fd6464ac5e20a62d3dc63bf72dbfe9db
marcelomorales <lmoral10@jhu.edu>
2022-11-08 22:59:00 -0500

    push dune and project structure

---
+++ src/strter.mli
@@ -0,0 +1,5 @@
+type 'a t (* hide the type 'a list here by not giving it in signature *)
+val emptyset : 'a t
+val add: 'a -> 'a t ->'a t
+val remove : 'a -> 'a t ->  ('a -> 'a -> bool) -> 'a t
+val contains: 'a -> 'a t ->  ('a -> 'a -> bool) -> bool    
