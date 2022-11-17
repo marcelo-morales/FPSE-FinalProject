e1c0b7872ddae6a000d11778b1824df09bb55ffc
marcelomorales <lmoral10@jhu.edu>
2022-11-16 20:02:32 -0500

    push changes

---
+++ src/learning.mli
@@ -0,0 +1,21 @@
+(* Machine Learning Classifcation *)
+
+open Torch
+
+
+val strings_from_file: string -> string
+val sanitize:  string -> string option
+val remove_first_character: string -> string
+val apply_remove_first: string list -> string list
+val make_biglist: string list -> string list list
+
+
+val read_image_from_file: string -> Tensor.t
+val predictImage: Tensor.t -> int
+
+(* Maybe if time *)
+val save_model: Tensor.t -> unit
+val load_model: unit
+
+
+
