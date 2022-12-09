3204a1aa0b04eb165e3c07502ae07333d051731c
marcelomorales <lmoral10@jhu.edu>
2022-12-09 12:21:02 -0500

    dune build and dune test works from outermost directory

--- backend/tests/tests.ml
+++ backend/tests/tests.ml
@@ -1,41 +1,31 @@
+[@@@ocaml.warning "-33"]
+[@@@ocaml.warning "-32"]
+[@@@ocaml.warning "-27"]
+[@@@ocaml.warning "-26"]
+
 
 (* open Core *)
 open OUnit2
 
-(* open Core *)
+open Core
 open Predict_conv
-
-
-(* Predict_conv.ml is the only file we are testing *)
-
-(* let path = Core_unix.getcwd () 
-
-let () = print_endline path *)
-
-(*
-
-
-let fixedpath = String.slice path (0) (-28 + String.length path);;
-
-let image_name_and_path = fixedpath^"backend/tests/"^"handwrittenImageOfOne.txt"
-let weights_name_and_path = fixedpath^"backend/tests/"^"weights" *)
-
 
 
 let image_name_and_path = "../../../../backend/tests/handwrittenImageOfOne.txt"
 let weights_name_and_path = "../../../../backend/tests/weights"
 
+let prediction1 = (predictImageFromFileName image_name_and_path weights_name_and_path)
+
 
 let miscellaneous_tests _ =
-  assert_equal 1 (predictImageFromFileName image_name_and_path weights_name_and_path)
-
+  assert_equal 1 prediction1
 
 
 
 let ml_tests =
   "Part 1" >: test_list [ "miscellaneous_ml_tests" >:: miscellaneous_tests ]
 
-let series = "FinalProject Tests" >::: [ ml_tests ]
+ let series = "FinalProject Tests" >::: [ ml_tests ]
 let () = run_test_tt_main series
 
 
